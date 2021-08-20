59d4b5b0062b0832d4825332bb077d89f24de556
mtfcd <mtfcd_cc@126.com>
2021-08-20 08:30:05 +0800

    nucleotide-count

--- nucleotide-count/src/lib.rs
+++ nucleotide-count/src/lib.rs
@@ -1,16 +1,34 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, iter::repeat};
+
+
+const NUCLEOTIDES: [char; 4] = ['A', 'C', 'T', 'G'];
 
 pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
-    unimplemented!(
-        "How much of nucleotide type '{}' is contained inside DNA string '{}'?",
-        nucleotide,
-        dna
-    );
+    if !NUCLEOTIDES.contains(&nucleotide) {
+        return Err(nucleotide);
+    }
+    let mut count = 0;
+    for e in dna.chars() {
+        if !NUCLEOTIDES.contains(&e) {
+            return Err(e);
+        }
+        if e == nucleotide {
+            count += 1;
+        }
+    }
+    return Ok(count)
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
-    unimplemented!(
-        "How much of every nucleotide type is contained inside DNA string '{}'?",
-        dna
-    );
+    let mut counts: HashMap<char, usize> = NUCLEOTIDES.to_vec().into_iter().zip(repeat(0)).collect();
+    for nu in dna.chars() {
+        if !NUCLEOTIDES.contains(&nu) {
+            return Err(nu)
+        }
+        match counts.get_mut(&nu) {
+            Some(count) => *count += 1,
+            None => {counts.insert(nu, 1);}
+        }
+    }
+    Ok(counts)
 }
